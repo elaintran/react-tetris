8b0a169e7bef6a7e9873111a3194eafa7dc2fbfd
elaintran <elain.ttran@gmail.com>
2019-08-18 14:44:19 -0500

    restructured json file and created a random piece function

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import tetriminos from "../../tetriminos.json";
+
+const checkTetriminos = () => {
+    const pieceIndex = Math.floor(Math.random() * tetriminos.length);
+    console.log(tetriminos[pieceIndex]);
+}
 
 class Cell extends Component {
     render() {
         return (
-            <div>cell</div>
+            <div>
+                cell
+            </div>
         );
     }
 }
